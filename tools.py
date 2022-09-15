1beaa22a7d4e368972f7c8aeaf982bf3583d0568
nsczxf965 <elena.treit@data-craft.de>
2022-09-15 12:20:15 +0200

    beginn

--- tools.py
+++ tools.py
@@ -1,5 +1,23 @@
 import pandas as pd
 import yfinance as yf
+import sqlalchemy
+import configparser
+
+def connect_postgres():
+    "Verbindung zu lokaler Postgres-Datenbank"
+    # Zugangsdaten aus der config-Datei einlesen
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    user = config["local_postgres"]["DB_USER"]
+    pw = config["local_postgres"]["DB_PW"]
+    address = config["local_postgres"]["DB_ADDRESS"]
+    port = config["local_postgres"]["DB_PORT"]
+    name = config["local_postgres"]["DB_NAME"]
+    # Verbindung zu Datenbank aufbauen
+    con_str = f"postgresql://{user}:{pw}@{address}:{port}/{name}"
+    con = sqlalchemy.create_engine(con_str)
+    return con
+
 
 def insert_symbol(con, ticker):
     # Daten aus Ticker auslesen
